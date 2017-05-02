93bf1ebc45b4c1140a3623a8d87302c587ce1d73
Antal <antal.sinkovits@gmail.com>
2017-05-02 21:07:48 +0200

    Base receipt data

--- src/main/java/com/sinkovits/rent/generator/GeneratorApp.java
+++ src/main/java/com/sinkovits/rent/generator/GeneratorApp.java
@@ -6,8 +6,63 @@ public class GeneratorApp {
 		new GeneratorApp().execute();
 	}
 
+	private String header;
+	private String date;
+	private String landlord;
+	private int rentValue;
+	private String rentValueText;
+	private String tenant;
+
 	public void execute() {
 
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getLandlord() {
+		return landlord;
+	}
+
+	public void setLandlord(String landlord) {
+		this.landlord = landlord;
+	}
+
+	public int getRentValue() {
+		return rentValue;
+	}
+
+	public void setRentValue(int rentValue) {
+		this.rentValue = rentValue;
+	}
+
+	public String getRentValueText() {
+		return rentValueText;
+	}
+
+	public void setRentValueText(String rentValueText) {
+		this.rentValueText = rentValueText;
+	}
+
+	public String getTenant() {
+		return tenant;
+	}
+
+	public void setTenant(String tenant) {
+		this.tenant = tenant;
 	}
 
 }
--- src/test/java/com/sinkovits/rent/generator/GeneratorAppTest.java
+++ src/test/java/com/sinkovits/rent/generator/GeneratorAppTest.java
@@ -1,5 +1,8 @@
 package com.sinkovits.rent.generator;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,6 +18,33 @@ public class GeneratorAppTest {
 	@Test
 	public void canApplicationExecute() {
 		app.execute();
+	}
+
+	@Test
+	public void canSetBaseData() {
+		// Given
+		String testDate = "2010.01.01";
+		String testHeader = "Test invoice";
+		String testLandlord = "John Doe";
+		int testRentValue = 200;
+		String testRentValueText = "Two hundred";
+		String testTenant = "Jane Doe";
+
+		// When
+		app.setDate(testDate);
+		app.setHeader(testHeader);
+		app.setLandlord(testLandlord);
+		app.setRentValue(testRentValue);
+		app.setRentValueText(testRentValueText);
+		app.setTenant(testTenant);
+
+		// Then
+		assertThat(app.getDate(), equalTo(testDate));
+		assertThat(app.getHeader(), equalTo(testHeader));
+		assertThat(app.getLandlord(), equalTo(testLandlord));
+		assertThat(app.getRentValue(), equalTo(testRentValue));
+		assertThat(app.getRentValueText(), equalTo(testRentValueText));
+		assertThat(app.getTenant(), equalTo(testTenant));
 	}
 
 }
